357b39cd7c030f0f38181cf88f03d5cec7cd8fad
BaoGeist <linofbaoze@outlook.com>
2025-10-26 11:19:35 -0400

    feat: add exports to index.jsx

--- packages/primitives/src/index.jsx
+++ packages/primitives/src/index.jsx
@@ -1,7 +1,7 @@
 export { WidgetShell } from "./WidgetShell.jsx";
 export { Text } from "./Text.jsx";
 export { Icon } from "./Icon.jsx";
-export { LineChart } from "./LineChart.jsx";
+export { Sparkline } from "./Sparkline.jsx";
 export { AppLogo } from "./AppLogo.jsx";
 export { MapImage } from "./MapImage.jsx";
 export { Image } from "./Image.jsx";
@@ -9,5 +9,11 @@ export { Checkbox } from "./Checkbox.jsx";
 export { Indicator } from "./Indicator.jsx";
 export { Divider } from "./Divider.jsx";
 export { Sparkline } from "./Sparkline.jsx";
+
+export { LineChart } from "./LineChart.jsx";
 export { BarChart } from "./BarChart.jsx";
+export { StackedBarChart } from "./StackedBarChart.jsx";
 export { RadarChart } from "./RadarChart.jsx";
+export { PieChart } from "./PieChart.jsx";
+export { ProgressBar } from "./ProgressBar.jsx";
+export { ProgressRing } from "./ProgressRing.jsx";
